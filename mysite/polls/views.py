fbc9aaace8925ede74cebaab72177f3afc03aa3e
aletterofthealphabet <wolfang393@gmail.com>
2021-06-28 12:30:25 -0700

    Completing Lab 8

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+# Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the result of question %s."
@@ -13,4 +24,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on questions %s." % question_id)
-# Create your views here.
